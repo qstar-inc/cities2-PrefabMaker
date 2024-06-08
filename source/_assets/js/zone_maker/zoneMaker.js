81b32087bcde46fa573fb6d8146a340cb3c462b5
Qoushik <qstar.inc@gmail.com>
2024-06-09 01:35:31 +0600

    added missing function for download button in zoneMaker

--- source/_assets/js/zone_maker/zoneMaker.js
+++ source/_assets/js/zone_maker/zoneMaker.js
@@ -423,5 +423,19 @@ export function create_prefab_file(line_values) {
     .replaceAll("{isOffice}", isOffice);
 }
 
+function downloadFile() {
+  const name = document.getElementById("name").value;
+  const content = document.getElementById("output").textContent;
+  const filename = `${shortname}.Prefab`;
+  const blob = new Blob([content], { type: "text/plain" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+window.downloadFile = downloadFile;
+
 setupColorPicker("hexInput1", "colorPicker1");
 setupColorPicker("hexInput2", "colorPicker2");
